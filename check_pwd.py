5eb6fbe3f85c51d80f273e60db7673a5c8523f06
Andrew <andrew.eppinger@yahoo.com>
2021-05-09 20:26:32 -0400

    Creates the check_pwd function in check_pwd.py (Does not do anything, just returns None). Also creates an empty test suite (singular test that simply uses "pass").

--- check_pwd.py
+++ check_pwd.py
@@ -1,3 +1,6 @@
 # Andrew Eppinger
 # CS362 Software Engineering II - Spring 2021
 # A2: TDD Hands On - Check password code
+
+def check_pwd(password):
+    return
--- tests.py
+++ tests.py
@@ -2,3 +2,14 @@
 # CS362 Software Engineering II - Spring 2021
 # A2: TDD Hands On - Unit tests
 
+import unittest
+from unittest import TestCase
+from check_pwd import check_pwd
+
+
+class Test(TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()

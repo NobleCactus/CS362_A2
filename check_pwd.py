c21613e32ac41b516992a9902fd5dc346f50a076
Andrew <andrew.eppinger@yahoo.com>
2021-05-10 17:35:58 -0400

    Adds a check to the "check_pwd" function that will return false if the length of the input is greater than 20 characters. The test now passes. Refactored the function by removing the check for inputs of length 0, as checking if inputs are less than 8 characters covers this. All tests pass.

--- check_pwd.py
+++ check_pwd.py
@@ -3,8 +3,8 @@
 # A2: TDD Hands On - Check password code
 
 def check_pwd(password):
-    if len(password) == 0:
-        return False
     if len(password) < 8:
+        return False
+    if len(password) > 20:
         return False
     return True

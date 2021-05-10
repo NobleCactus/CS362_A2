20520c8ef607d401542f8048c50cd10187ea219b
Andrew <andrew.eppinger@yahoo.com>
2021-05-10 17:49:33 -0400

    Adds a check for a lowercase character in the input. Returns False if none is found. Test now passes.

--- check_pwd.py
+++ check_pwd.py
@@ -3,10 +3,14 @@
 # A2: TDD Hands On - Check password code
 
 def check_pwd(password):
+    lowercase_letter_present = 0
     if len(password) < 8:
         return False
     if len(password) > 20:
         return False
+    for char in password:
+        if char.islower():
+            lowercase_letter_present = 1
+    if lowercase_letter_present == 0:
+        return False
     return True
-
-st = "HhjroOhjdsfRkdfhSkjergE"
